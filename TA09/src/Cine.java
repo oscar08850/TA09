8efd4857795bc494784a1141f92d78ec46e75e04
facusilva <facun772@gmail.com>
2022-10-07 11:54:28 +0200

    Commit Facundo: método asignarAsiento v1 e instanciar objetos en main

--- TA09/src/Cine.java
+++ TA09/src/Cine.java
@@ -1,17 +1,23 @@
-import java.util.ArrayList;
+import java.util.Random;
 
 public class Cine {
 	
-	protected Pelicula pelicula;
+	private Pelicula pelicula;
 	
-	protected double precio;
+	private double precio;
 	
-	protected Espectador asientos[][];
+	private Espectador asientos[][];
+	
+	private int asientosOcupados;
+	
+	private boolean lleno;
 
 	public Cine(Pelicula pelicula, double precio) {
 		this.pelicula = pelicula;
 		this.precio = precio;
 		this.asientos = new Espectador[8][9];
+		this.asientosOcupados = 0;
+		this.lleno = false;
 	}
 
 	public Pelicula getPelicula() {
@@ -38,6 +44,40 @@ public class Cine {
 		this.asientos = asientos;
 	}
 	
+	public String asignarAsiento(Espectador e) {
+		
+		if(this.lleno) {
+			return "Lo sentimos, la sala está llena.";
+		}else {
+			if( e.getEdad() < this.pelicula.getEdadMinima() ) {
+				return "Lo sentimos, tienes "+e.getEdad()+" años y necesitas tener "+this.pelicula.getEdadMinima()+" años para ver esta película" ;
+			}
+			if( e.getDinero() < this.getPrecio() ) {
+				return "Lo sentimos, tienes "+e.getDinero()+" euros y necesitas "+this.getPrecio()+" para la entrada";
+			}
+			
+			boolean continuar = true;
+			Random rand = new Random();
+			int x = 0;
+			int y = 0;
+			String letras = "ABCDEFGHI";
+			while(continuar) {
+				x = rand.nextInt(8);
+				y = rand.nextInt(9);
+				if(this.asientos[x][y] == null) {
+					continuar = false;
+					this.asientos[x][y] = e;
+					this.asientosOcupados++;
+					if(this.asientosOcupados == (8*9)) {
+						lleno = true;
+					}
+				}	
+			}
+			return "Te hemos asignado el asiento: "+x+" "+letras.charAt(y);
+		}
+		
+		
+	}
 	
 			
 	
--- TA09/src/mainAPP.java
+++ TA09/src/mainAPP.java
@@ -3,6 +3,15 @@ public class mainAPP {
 
 	public static void main(String[] args) {
 
+		Espectador e1 = new Espectador("Pepito Grillo", 18, 20.20);
+		
+		Pelicula p1 = new Pelicula("Malditos Bastardos", 180, 18, "Quentin Tarantino");
+		
+		Cine c1 = new Cine(p1, 10.50);
+		
+		System.out.println(c1.asignarAsiento(e1));
+		
+		
 	}
 
 }
