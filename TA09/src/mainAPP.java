cf1958148da7c12c78b1d6bd6be791748495703d
Chals <csanchezbasket@gmail.com>
2022-10-11 13:26:24 +0200

    Comentarios y retoques finales

--- TA09/src/mainAPP.java
+++ TA09/src/mainAPP.java
@@ -1,5 +1,3 @@
-import clases.Cine;
-import clases.Espectador;
 import clases.Pelicula;
 import clases.Sala;
 import clases.FuncionesAuxiliares;
@@ -8,18 +6,12 @@ public class mainAPP {
 
 	public static void main(String[] args) {
 
-		//Espectador e1 = new Espectador("Pepito Grillo", 18, 20.20);
-
 		Pelicula p1 = new Pelicula("Malditos Bastardos", 180, 18, "Quentin Tarantino");
 
 		Sala c1 = new Sala(p1, 10.50);
-
-//		System.out.println(c1.asignarAsiento(e1));
-		String pruebaNombre = "";
 		
-		FuncionesAuxiliares aux = new FuncionesAuxiliares();
 		
-		aux.generaEspectadores(c1);
+		FuncionesAuxiliares.generaEspectadores(c1);
 		
 
 	}
