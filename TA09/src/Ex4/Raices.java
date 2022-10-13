e93b5902e12e858d0baec92421875a57980e632e
facusilva <facun772@gmail.com>
2022-10-13 21:35:36 +0200

    añadidos comentarios ex4

--- TA09/src/Ex4/Raices.java
+++ TA09/src/Ex4/Raices.java
@@ -4,22 +4,26 @@ import java.util.ArrayList;
 
 public class Raices {
 	
+	//atributos
 	private double a;
 	
 	private double b;
 	
 	private double c;
 
+	//declaro el constructor
 	public Raices(double a, double b, double c) {
 		this.a = a;
 		this.b = b;
 		this.c = c;
 	}
 	
+	//me devuelve el discriminante
 	public double getDiscriminante() {
 		return (Math.pow(this.b, 2))-4*this.a*this.c;
 	}
 	
+	//si tiene dos posibles soluciones (discriminante mayor que 0) devuelve true, de lo contrario devuelve false
 	public boolean tieneRaices() {
 		if(this.getDiscriminante() > 0) {
 			return true;
@@ -28,6 +32,7 @@ public class Raices {
 		}
 	}
 	
+	//si tiene una sola posible solución (discriminante igual a 0) devuelve true, de lo contrario devuelve false
 	public boolean tieneRaiz() {
 		if(this.getDiscriminante() == 0) {
 			return true;
@@ -36,24 +41,27 @@ public class Raices {
 		}
 	}
 	
+	//calcula el resultado/s de la ecuación si es posible y lo/s muestra por pantalla, si no hay resultado indica que no tiene solucion
 	public void calcular() {
 		double a = this.a;
 		double b = this.b;
 		double c = this.c;
 		ArrayList<Double> soluciones = new ArrayList<Double>();
+		//si tiene dos soluciones
 		if(this.tieneRaices()) {
 			soluciones.add( ((-b) + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a) );
 			soluciones.add( ((-b) - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a) );
 			System.out.println("Solución 1 = "+soluciones.get(0));
 			System.out.println("Solución 2 = "+soluciones.get(1));
-		}else if(this.tieneRaiz()) {
+		}else if(this.tieneRaiz()) { //else if tiene una sola solución
 			soluciones.add( ((-b) - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a) );
 			System.out.println("Solución = "+soluciones.get(0));
-		}else {
+		}else { //else no tiene solución
 			System.out.println("No hay soluciones para esta ecuación");
 		}
 	}
 	
+	//muestra por pantalla las soluciones, en caso de tener solo 1 solución o ninguna devuelve error
 	public void obtenerRaices() {
 		ArrayList<Double> soluciones = new ArrayList<Double>();
 		if(this.tieneRaices()) {
@@ -66,6 +74,7 @@ public class Raices {
 		}
 	}
 	
+	//muestra por pantalla la solución, en caso de tener 2 soluciones o ninguna solución devuelve error
 	public void obtenerRaiz() {
 		ArrayList<Double> soluciones = new ArrayList<Double>();
 		if(this.tieneRaiz()) {
--- TA09/src/Ex4/mainApp.java
+++ TA09/src/Ex4/mainApp.java
@@ -3,11 +3,13 @@ package Ex4;
 public class mainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//instancio los objetos Raices
 		Raices raiz1 = new Raices(-5, 20, 15);
 		Raices raiz2 = new Raices(1, -4, 4);
+		//muestro el discriminante
 		System.out.println(raiz1.getDiscriminante());
 		System.out.println(raiz2.getDiscriminante());
+		//llamo a los métodos obtenerRaices() y obtenerRaiz() para cada uno de los objetos instanciados
 		raiz1.obtenerRaices();
 		raiz1.obtenerRaiz();
 		raiz2.obtenerRaices();
