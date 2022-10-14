bac0ff93f838926f8ebb980df92eb3bfab730267
Chals <csanchezbasket@gmail.com>
2022-10-14 15:49:39 +0200

    Commit Carlos: Ejercicio 3 comentado

--- TA09/src/Ex3/FuncionesAuxiliares.java
+++ TA09/src/Ex3/FuncionesAuxiliares.java
@@ -2,13 +2,13 @@ package Ex3;
 
 public class FuncionesAuxiliares {
 	
-	public static String masPaginas(Libro l1, Libro l2){
+	public static String masPaginas(Libro l1, Libro l2){	//funcion para calcular que libro tiene mas paginas
 		
-		if(l1.getPaginas()>l2.getPaginas()) {
+		if(l1.getPaginas()>l2.getPaginas()) {	//comparamos las paginas de los libros pasados con su getter
 			return l1.getTitulo();
 		}else if(l1.getPaginas()<l2.getPaginas()) {
 			return l2.getTitulo();
-		}else return "Ambos libros tienen las mismas páginas";
+		}else return "Ambos libros tienen las mismas paginas";
 		
 	}
 
